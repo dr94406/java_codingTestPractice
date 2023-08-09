4f7f8f19d9f6ee063024d8a192ae3ad0ced1ac1c
khm <dr94406@naver.com>
2023-08-09 23:30:58 +0900

    [feat] 격자판 최대합, 등수 구하기 문제 추가

--- src/java/Arrays/FindStudentRank.java
+++ src/java/Arrays/FindStudentRank.java
@@ -3,8 +3,16 @@ package Arrays;
 import java.util.Scanner;
 
 public class FindStudentRank {
-    public int solution() {
-        int answer = 0;
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+           int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if(arr[j] > arr[i]) cnt++;
+            }
+            answer[i] = cnt;
+
+        }
 
         return answer;
     }
@@ -12,5 +20,11 @@ public class FindStudentRank {
     public static void main(String[] args) {
         FindStudentRank T = new FindStudentRank();
         Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for(int x : T.solution(n, arr)) System.out.print(x + " ");
     }
 }
